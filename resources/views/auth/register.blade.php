f0dac43807491c39464ffecac5b5c051e7929b74
hhuezo <hugo.alex.huezo@gmail.com>
2023-07-29 12:43:32 -0600

    Merge branch 'main' hhuezo

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -118,14 +118,20 @@
                                 <div class="flex-1">
                                     <div class="card-title text-slate-900 dark:text-white">Registro</div>
                                 </div>
+                                <a href="{{ url('login') }}">
+                                    <button class="btn btn-dark float-right">
+                                        <iconify-icon icon="icon-park-solid:back" style="color: white;" width="18">
+                                        </iconify-icon>
+                                    </button>
+                                </a>
                             </header>
                             <form method="POST" action="{{ route('register') }}">
-                            @csrf
+                                @csrf
 
                                 <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-7">
                                     <div class="input-area relative">
                                         <label for="Nombre" class="form-label">Nombre</label>
-                                        <input type="text"  name="Nombre" id="Nombre" class="form-control">
+                                        <input type="text" name="Nombre" id="Nombre" class="form-control">
                                     </div>
                                     <div class="input-area relative">
                                         <label for="Apellido" class="form-label">Apellido</label>
@@ -157,8 +163,10 @@
                                         <select name="Nacionalidad" id="Nacionalidad" class="form-control">
                                             <option value="Salvadoreño" class="dark:bg-slate-700">Salvadoreño</option>
                                             <option value="Panameño" class="dark:bg-slate-700">Panameño</option>
-                                            <option value="Nacionalidad 3" class="dark:bg-slate-700">Nacionalidad 3</option>
-                                            <option value="Nacionalidad 4" class="dark:bg-slate-700">Nacionalidad 4</option>
+                                            <option value="Nacionalidad 3" class="dark:bg-slate-700">Nacionalidad 3
+                                            </option>
+                                            <option value="Nacionalidad 4" class="dark:bg-slate-700">Nacionalidad 4
+                                            </option>
                                         </select>
                                     </div>
                                     <div class="input-area relative">
@@ -169,7 +177,9 @@
                                         <label for="Pais" class="form-label">Pais</label>
                                         <select name="Pais" id="Pais" class="form-control">
                                             @foreach ($paises as $obj)
-                                            <option value="{{$obj->Id}}" class="dark:bg-slate-700" {{ old('Pais') == $obj->Id ? 'selected' : '' }}>{{$obj->Nombre}}</option>
+                                                <option value="{{ $obj->Id }}" class="dark:bg-slate-700"
+                                                    {{ old('Pais') == $obj->Id ? 'selected' : '' }}>{{ $obj->Nombre }}
+                                                </option>
                                             @endforeach
                                         </select>
                                     </div>
@@ -177,7 +187,9 @@
                                         <label for="Departamento" class="form-label">Departamento</label>
                                         <select name="Departamento" id="Departamento" class="form-control">
                                             @foreach ($departamentos as $obj)
-                                            <option value="{{$obj->Id}}" class="dark:bg-slate-700" {{ old('Departamento') == $obj->Id ? 'selected' : '' }}>{{$obj->Nombre}}</option>
+                                                <option value="{{ $obj->Id }}" class="dark:bg-slate-700"
+                                                    {{ old('Departamento') == $obj->Id ? 'selected' : '' }}>
+                                                    {{ $obj->Nombre }}</option>
                                             @endforeach
                                         </select>
                                     </div>
@@ -185,7 +197,9 @@
                                         <label for="Municipio" class="form-label">Municipio</label>
                                         <select name="Municipio" id="Municipio" class="form-control">
                                             @foreach ($municipios as $obj)
-                                            <option value="{{$obj->Id}}" class="dark:bg-slate-700" {{ old('Municipio') == $obj->Id ? 'selected' : '' }}>{{$obj->Nombre}}</option>
+                                                <option value="{{ $obj->Id }}" class="dark:bg-slate-700"
+                                                    {{ old('Municipio') == $obj->Id ? 'selected' : '' }}>
+                                                    {{ $obj->Nombre }}</option>
                                             @endforeach
                                         </select>
                                     </div>
@@ -194,10 +208,14 @@
                                         <input type="tel" name="Telefono" id="Telefono" class="form-control">
                                     </div>
                                     <div class="input-area relative">
-                                        <label for="EntidadCertificadora" class="form-label">Entidad Certificadora</label>
-                                        <select name="EntidadCertificadora" id="EntidadCertificadora" class="form-control">
+                                        <label for="EntidadCertificadora" class="form-label">Entidad
+                                            Certificadora</label>
+                                        <select name="EntidadCertificadora" id="EntidadCertificadora"
+                                            class="form-control">
                                             @foreach ($entidades as $obj)
-                                            <option value="{{$obj->Id}}" class="dark:bg-slate-700" {{ old('EntidadCertificadora') == $obj->Id ? 'selected' : '' }}>{{$obj->Nombre}}</option>
+                                                <option value="{{ $obj->Id }}" class="dark:bg-slate-700"
+                                                    {{ old('EntidadCertificadora') == $obj->Id ? 'selected' : '' }}>
+                                                    {{ $obj->Nombre }}</option>
                                             @endforeach
                                         </select>
                                     </div>
@@ -205,24 +223,31 @@
                                         <label for="TipoCertificado" class="form-label">Tipo Certificado</label>
                                         <select name="TipoCertificado" id="TipoCertificado" class="form-control">
                                             @foreach ($tipos_certificados as $obj)
-                                            <option value="{{$obj->Id}}" class="dark:bg-slate-700" {{ old('TipoCertificado') == $obj->Id ? 'selected' : '' }}>{{$obj->Descripcion}}</option>
+                                                <option value="{{ $obj->Id }}" class="dark:bg-slate-700"
+                                                    {{ old('TipoCertificado') == $obj->Id ? 'selected' : '' }}>
+                                                    {{ $obj->Descripcion }}</option>
                                             @endforeach
                                         </select>
                                     </div>
                                     <div class="input-area relative">
-                                        <label for="NumeroCertificacion" class="form-label">Numero de Certificación</label>
-                                        <input type="text" name="NumeroCertificacion" id="NumeroCertificacion" class="form-control">
+                                        <label for="NumeroCertificacion" class="form-label">Numero de
+                                            Certificación</label>
+                                        <input type="text" name="NumeroCertificacion" id="NumeroCertificacion"
+                                            class="form-control">
                                     </div>
                                     <div class="input-area relative">
-                                        <label for="VigenciaCertificacion" class="form-label">Vigencia de la Certificación</label>
-                                        <input type="date" name="VigenciaCertificacion" id="VigenciaCertificacion" class="form-control">
+                                        <label for="VigenciaCertificacion" class="form-label">Vigencia de la
+                                            Certificación</label>
+                                        <input type="date" name="VigenciaCertificacion" id="VigenciaCertificacion"
+                                            class="form-control">
                                     </div>
 
 
                                 </div>
                                 <br>
                                 <div style="text-align: right;">
-                                    <button type="submit" class="btn inline-flex justify-center btn-dark">Registrar</button>
+                                    <button type="submit"
+                                        class="btn inline-flex justify-center btn-dark">Registrar</button>
                                 </div>
 
 
