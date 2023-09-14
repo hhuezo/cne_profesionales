a5b00c106af2393eae0dbcc2317012778903b4bb
Zirok9511 <zirokguadron11@gmail.com>
2023-09-13 18:58:09 -0600

    for review

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,19 +19,38 @@
             <div class="col">
                 <div class=" w-auto">Lunes 25 de Septiembre de 2023</div>
             </div>
-            <div class="col">
-                <div class="border border-white mx-3 w-auto page rounded ps-2">Indentificarse</div>
+            <div class="col text-center">
+                <button type="button" class="btn btn-info text-white"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-person" viewBox="0 0 16 16">
+                        <path d="M12 1a1 1 0 0 1 1 1v10.755S12 11 8 11s-5 1.755-5 1.755V2a1 1 0 0 1 1-1h8zM4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4z" />
+                        <path d="M8 10a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                    </svg> Identificarse</button>
             </div>
-            <div class="col">
-                <div class="border border-white mx-3 w-auto page rounded ps-2">Registrarse</div>
+            <div class="col text-center">
+                <button type="button" class="btn btn-info text-white"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-add" viewBox="0 0 16 16">
+                        <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0Zm-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z" />
+                        <path d="M8.256 14a4.474 4.474 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10c.26 0 .507.009.74.025.226-.341.496-.65.804-.918C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4s1 1 1 1h5.256Z" />
+                    </svg> Registrarse</button>
             </div>
-            <div class="col">
-                <div class="border border-white mx-3 w-auto page rounded ps-2"><box-icon name='search' ></box-icon>BUSCAR</div>
+            <div class="col text-center">
+                <div class="input-group rounded">
+                    <input type="search" class="form-control rounded" placeholder="Buscar" aria-label="Buscar" aria-describedby="search-addon" />
+                    <span class="input-group-text border-0" id="search-addon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                        </svg>
+                    </span>
+                </div>
             </div>
         </div>
-        <div class="row w-100 page ps-5">
-            <div class="text-left">
-                <h3>TESTING</h3>
+        <div class="row w-100 py-3">
+            <div class="col text-center content1"><img src="img/Para_web.png" alt="" class="img-fluid"></div>
+            <div class="col text-center"><img src="img/image15.jpeg" alt="" class="img-fluid"></div>
+            <div class="col text-center content1"><img src="img/image16.jpeg" alt="" class="img-fluid"></div>
+            <div class="col text-center content1"><img src="img/image17.png" alt="" class="img-fluid"></div>
+        </div>
+        <div class="row w-100 page2 ps-5">
+            <div class="text-center text-white">
+                <h3>REGISTRO DE PERSONAS CERTIFICADAS</h3>
             </div>
         </div>
         <div class="row w-100 bg-black text-white ps-5 py-3">
@@ -60,5 +79,25 @@
 <style>
     .page {
         background-color: #2a6099;
+    }
+    .page2{
+        background-color: #92d050;
+    }
+
+    .btn-info {
+        background-color: #2a6099;
+        border: solid 1px #2a6099;
+    }
+
+    .btn:hover {
+        background-color: white !important;
+        color: black !important;
+        border: 1px solid black;
+    }
+
+    .content1 {
+        display: flex;
+        align-items: center;
+        flex-wrap: wrap;
     }
 </style>
