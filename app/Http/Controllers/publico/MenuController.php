4764b346b6a100b4a30adb52221d593b37af984c
hhuezo <hugo.alex.huezo@gmail.com>
2023-09-22 06:20:55 -0600

    redireccion menu inicio

--- app/Http/Controllers/publico/MenuController.php
+++ app/Http/Controllers/publico/MenuController.php
@@ -7,9 +7,33 @@ use App\Http\Controllers\Controller;
 class MenuController extends Controller
 {
     //menu_inicio
-    public function menu_inicio()
+    public function menu_flujo()
     {
 
         return view('inicio.flujo');
+    }
+
+    public function menu_requisitos()
+    {
+
+        return view('inicio.requisitos');
+    }
+
+    public function menu_perfil()
+    {
+
+        return view('inicio.perfil');
+    }
+
+    public function menu_contenido_formativo()
+    {
+
+        return view('inicio.contenido_formativo');
+    }
+
+    public function menu_unidades_formativas()
+    {
+
+        return view('inicio.unidades_formativas');
     }
 }
