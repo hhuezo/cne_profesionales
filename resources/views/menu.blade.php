b05793f7559f307631e98517aacc4b0766a9ea0a
hhuezo <hugo.alex.huezo@gmail.com>
2023-10-16 07:25:34 -0600

    eliminar pie de pagina

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -2090,7 +2090,7 @@
             </div>
 
             <!-- BEGIN: Footer For Desktop and tab -->
-            <footer class="md:block hidden" id="footer">
+            {{-- <footer class="md:block hidden" id="footer">
                 <div
                     class="site-footer px-6 bg-white dark:bg-slate-800 text-slate-500 dark:text-slate-300 py-4 ltr:ml-[248px] rtl:mr-[248px]">
                     <div class="grid md:grid-cols-2 grid-cols-1 md:gap-5">
@@ -2107,7 +2107,7 @@
                         </div>
                     </div>
                 </div>
-            </footer>
+            </footer> --}}
             <!-- END: Footer For Desktop and tab -->
             <div
                 class="bg-white bg-no-repeat custom-dropshadow footer-bg dark:bg-slate-700 flex justify-around items-center backdrop-filter backdrop-blur-[40px] fixed left-0 bottom-0 w-full z-[9999] bothrefm-0 py-[12px] px-4 md:hidden">
