5e9253d746da99722d6beec342b24f47ebdbb798
hhuezo <hugo.alex.huezo@gmail.com>
2023-11-05 15:45:45 -0600

    titulo lugares de formacion

--- resources/views/catalogo/lugar_formacion/index.blade.php
+++ resources/views/catalogo/lugar_formacion/index.blade.php
@@ -5,7 +5,7 @@
 
     <div class="card">
         <header class=" card-header noborder">
-            <h4 class="card-title">Lugar formación
+            <h4 class="card-title">Listado de lugares de formación
             </h4>
             <a href="{{ url('catalogo/lugar_formacion/create') }}">
                 <button class="btn btn-outline-dark">Nuevo</button>
