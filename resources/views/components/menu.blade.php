356c7df25afafd1719b220b6db38afa04c3f0143
hhuezo <hugo.alex.huezo@gmail.com>
2023-11-15 19:41:22 -0600

    menu drop

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -4,25 +4,29 @@
             <nav id="navbar" class="navbar">
                 <ul class="text-center">
                     @foreach ($menus as $menu)
-                        <li class="dropdown"><a
-                                href="{{ $menu->Snippet != null ? url('publico/pagina') . '/' . $menu->Snippet : '' }}"><span>{{ $menu->Descripcion }}</span>
-                                <i class="bi bi-chevron-down"></i></a>
-                            @if ($menu->sub_menus($menu->Id) > 0)
-                                <ul>
-                                    @foreach ($sub_menus as $sub_menu)
-                                        @if ($sub_menu->Antesesora == $menu->Id)
-                                            <a
-                                                href="{{ $sub_menu->Snippet != null ? url('publico/pagina') . '/' . $sub_menu->Snippet : '' }}">
-                                                <li class="dropdown" style="text-align: left"><span>{{ $sub_menu->Descripcion }}
-                                                    </span>
-                                                </li>
-                                            </a>
-                                        @endif
-                                    @endforeach
-                                </ul>
-                            @endif
+                        <a href="{{ $menu->Snippet != null ? url('publico/pagina') . '/' . $menu->Snippet : '' }}">
+                            <li class="dropdown">
 
-                        </li>
+                                <span>{{ $menu->Descripcion }}</span>
+                                <i class="bi bi-chevron-down"></i>
+                                @if ($menu->sub_menus($menu->Id) > 0)
+                                    <ul>
+                                        @foreach ($sub_menus as $sub_menu)
+                                            @if ($sub_menu->Antesesora == $menu->Id)
+                                                <a
+                                                    href="{{ $sub_menu->Snippet != null ? url('publico/pagina') . '/' . $sub_menu->Snippet : '' }}">
+                                                    <li class="dropdown" style="text-align: left">
+                                                        <span>{{ $sub_menu->Descripcion }}
+                                                        </span>
+                                                    </li>
+                                                </a>
+                                            @endif
+                                        @endforeach
+                                    </ul>
+                                @endif
+
+                            </li>
+                        </a>
                     @endforeach
                     <li class="dropdown"><a href="{{ url('publico/busqueda') }}"><span>Personas certificadas</span>
                         </a>
