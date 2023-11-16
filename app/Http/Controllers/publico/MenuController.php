d34bcd71d343332a4b91bb8a0b368ab5fdb9ebb9
hhuezo <hugo.alex.huezo@gmail.com>
2023-11-15 18:45:01 -0600

    uitar auth de menu

--- app/Http/Controllers/publico/MenuController.php
+++ app/Http/Controllers/publico/MenuController.php
@@ -11,10 +11,7 @@ use Illuminate\Http\Request;
 
 class MenuController extends Controller
 {
-    public function __construct()
-    {
-          $this->middleware('auth');
-    }
+
     public function index()
     {
         $menus = Menu::where('Antesesora','=',null)->get();
